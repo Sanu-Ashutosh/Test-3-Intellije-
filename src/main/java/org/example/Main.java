25c16cd6b4a93c43c7b39007f8b10877726cc03d
Ashutosh Pradhan <sanu.ashutoshpradhan@gmail.com>
2025-03-23 16:53:36 +0530

    First commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        for (int j = 0; j < 10; j++) {
+            System.out.println(10);
+        }
+    }
+}
