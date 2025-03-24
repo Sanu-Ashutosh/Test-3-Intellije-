2d4f86f6fe853967b3ce7e7ea8a08d15e64f21f9
Ashutosh Pradhan <sanu.ashutoshpradhan@gmail.com>
2025-03-24 16:17:15 +0530

    Update Check11.java
    New commit in main

--- src/main/java/org/example/Check11.java
+++ src/main/java/org/example/Check11.java
@@ -8,27 +8,5 @@ public class Check11 {
         return 1;
     }
 
-    public int b() {
-        System.out.println("b");
-
-        return 1;
-    }
-
-    public int c() {
-        System.out.println("c");
-        b();
-        return 1;
-    }
-
-    public int d() {
-        System.out.println("d");
-        c();
-        return 1;
-    }
-
-    public int e() {
-        System.out.println("e");
-        d();
-        return 1;
-    }
+   
 }
