f1ab57a6bf0c7ff41031b3afa49374a9122ae9ff
Ashutosh Pradhan <sanu.ashutoshpradhan@gmail.com>
2025-03-24 16:22:09 +0530

    Fifth  commit

--- src/main/java/org/example/Check11.java
+++ src/main/java/org/example/Check11.java
@@ -1,9 +1,11 @@
 package org.example;
 
+import java.util.Calendar;
+
 public class Check11 {
 
     public int a() {
-       Calendar c = Calendar.getInstance(); 
+       Calendar c = Calendar.getInstance();
         System.out.println("The Current Date is:" + c.getTime()); 
         return 1;
     }
