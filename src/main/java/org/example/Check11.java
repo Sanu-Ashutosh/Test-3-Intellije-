052e45c148edb6e48c5de298e1b400286d183e3b
Ashutosh Pradhan <sanu.ashutoshpradhan@gmail.com>
2025-03-24 15:57:39 +0530

    Third commit

--- src/main/java/org/example/Check11.java
+++ src/main/java/org/example/Check11.java
@@ -2,5 +2,34 @@ package org.example;
 
 public class Check11 {
 
+    public int a() {
+        System.out.println("a");
+        e();
 
+        return 1;
+    }
+
+    public int b() {
+        System.out.println("b");
+
+        return 1;
+    }
+
+    public int c() {
+        System.out.println("c");
+        b();
+        return 1;
+    }
+
+    public int d() {
+        System.out.println("d");
+        c();
+        return 1;
+    }
+
+    public int e() {
+        System.out.println("e");
+        d();
+        return 1;
+    }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,27 @@
 package org.example;
 
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        for (int j = 0; j < 10; j++) {
+        for (int j = 0; j < 1; j++) {
             System.out.println(10);
-            System.out.println("biusdk");
-            System.out.println("biusdk");
-            System.out.println("biusdk");
-            System.out.println("biusdk");
-            System.out.println("biusdk");
-            System.out.println("biusdk");
+            System.out.println("ashutosh");
+            System.out.println("ashutosh");
+            System.out.println("ashutosh");
+            System.out.println("ashutosh");
+            System.out.println("ashutosh");
+            System.out.println("ashutosh");
+
+
+            List<Object> sdfs = List.of("sdfs", 'd', "fwefwwe", 1, 23, 432l);
+            new Check11().a();
+
         }
     }
 }
