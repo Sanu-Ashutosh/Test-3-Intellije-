2653b6e49aba95100c6af3fb5d73f37c62f113da
Ashutosh Pradhan <sanu.ashutoshpradhan@gmail.com>
2025-03-31 19:02:41 +0530

    check1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,14 +9,9 @@ import java.util.Objects;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-       
-
-
-
-            List<Object> sdfs = List.of("sdfs", 'd', "fwefwwe", 1, 23, 432l);
-            System.out.println(sdfs);
-            new Check11().a();
-
-        
+        List<Object> sdfs = List.of("sdfs", 'd', "fwefwwe", 1, 23, 432l);
+        System.out.println(sdfs);
+        int check = new Check11().a();
+        System.out.println(check);
     }
 }
